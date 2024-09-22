a596b10613aa22668d00d21b38d90d8fd8e4f71b
Tsubaki-01 <wengqiang9198@gmail.com>
2024-09-22 21:23:39 +0800

    to Step 10

--- cmake-3.30.3-tutorial-source/Step10/MathFunctions/MathFunctions.h
+++ cmake-3.30.3-tutorial-source/Step10/MathFunctions/MathFunctions.h
@@ -1,3 +1,15 @@
-namespace mathfunctions {
-double sqrt(double x);
+#if defined(_WIN32)
+#  if defined(EXPORTING_MYMATH)
+#    define DECLSPEC __declspec(dllexport)
+#  else
+#    define DECLSPEC __declspec(dllimport)
+#  endif
+#else // non windows
+#  define DECLSPEC
+#endif
+
+
+namespace mathfunctions
+{
+    double sqrt(double x);
 }
